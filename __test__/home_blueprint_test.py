e4087ce9bc0a0d51200d015b21851f1d4a7080a9
Carlos Peter <ccppeter@protonmail.com>
2022-11-01 11:29:09 -0300

    first test

---
+++ __test__/home_blueprint_test.py
@@ -0,0 +1,14 @@
+from ward import test
+from splinter import Browser
+from app import create_app
+
+
+@test("página principal deve estar online")
+def _():
+    app = create_app()
+    app.testing = True
+
+    browser = Browser("flask", app=app)
+
+    browser.visit("/")
+    assert browser.is_text_present("Olá, Flask!")
